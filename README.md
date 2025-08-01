e6829a68836701af77ba70799d1a3089126eb0fb
Maren Strobl <maren.strobl@cedis.fu-berlin.de>
2025-08-01 19:03:10 +0200

    Release version 1.3.0

--- README.md
+++ README.md
@@ -1,72 +1,75 @@
-# Open Encyclopedia System Plugin
+# OES Media Module
+Welcome to the Open Encyclopedia System (OES) Media repository on GitHub.  
+OES is a modular and configurable software platform for creating, publishing, and maintaining online encyclopedias in the humanities and social sciences. It is designed to be accessible worldwide through Open Access.
 
-Welcome to the Open Encyclopedia System Media (OES Media) repository on GitHub. OES is a modular and configurable software for creating, publishing and maintaining online encyclopedias in the humanities and social sciences that are accessible worldwide via Open Access. For more information please visit the main repository or our website.
+For more information, please visit the [main repository](https://github.com/open-encyclopedia-system) or our [website](https://open-encyclopedia-system.org).
 
-A typical OES application consists of an OES plugin and an additional OES project plugin which implements the OES features for the application. The OES Media plugin is an additional OES module to include media blocks and functionalities.
+A typical OES application consists of:
+- the **OES Core plugin**
+- a **project-specific OES plugin** that implements application-specific features
+- optional **OES modules**, such as this module
 
-## Documentation (Coming Soon)
+The **OES Media** plugin provides reusable blocks and media-related functionality for use in OES-based projects.
 
-We are working on a detailed user manual and a technical function reference. Support is currently provided via our email help desk info@open-encyclopedia-system.org. We answer questions related to the OES plugin and its usage.
+⚠️ **Deprecation Notice**  
+This module contains legacy code. As of **OES Core Plugin version 2.4.0**, its functionality has been replaced by the media blocks included in the core plugin.
+We recommend using the latest version of **OES Core** for media-related features.
 
+## Dependencies
+This module depends on:
 
-## Getting Started
+- **OES Core**, version `2.3.3`  
+  Repository: [https://github.com/open-encyclopedia-system/oes-core](https://github.com/open-encyclopedia-system/oes-core)
 
-### Dependencies
-
-OES Media depends on the OES Core Plugin:
-* OES Core, version 2.3.3, URL: https://github.com/open-encyclopedia-system/oes-core.
-
-OES Media depends on the WordPress plugin ACF PRO:
-* Advanced Custom Fields, version 6.3.4, URL: https://www.advancedcustomfields.com/pro/.
-
-
-### Installation
-
-1. Download the OES plugin from gitHub and add it to your WordPress plugin directory.
-2. Download and activate the dependencies.
-3. Activate the OES plugin.
-4. Create your OES project plugin or download and activate the OES Demo plugin.
-5. (Optional) Download and activate the OES theme.
-6. Download and activate the OES Media module.
-
-If the installation was successful you will now see the menu "OES Settings" and when using the block editor you can select the blocks "OES Panel", "OES Image Panel" and "OES Gallery Panel". The OES Media module and its functionalities are now available inside your WordPress installation.
-
-You can start configuring by exploring the OES settings (documentation coming soon) or editing the model.json inside your project plugin.
+- **Advanced Custom Fields (ACF Pro)**, version `6.3.4`  
+  Website: [https://www.advancedcustomfields.com](https://www.advancedcustomfields.com)
 
 ## Support
+This repository does **not** offer public support or issue tracking.  
+If you need help using the OES plugins, please contact our help desk:  
+**info@open-encyclopedia-system.org**
 
-This repository is not suitable for support.
+For information about available modules, customization options, or help launching your own encyclopedia, visit:  
+[https://open-encyclopedia-system.org](https://open-encyclopedia-system.org)
 
-Support is currently provided via our email help desk info@open-encyclopedia-system.org. We answer questions related to the OES plugin and its usage. For further information about online encyclopedias and possible customization please visit our [website](http://www.open-encyclopedia-system.org/).
-
+## Documentation
+The full user and technical manual is available at:  
+[https://manual.open-encyclopedia-system.org/](https://manual.open-encyclopedia-system.org/)
 
 ## Contributing
+If you are interested in contributing to OES development, please get in touch:  
+**info@open-encyclopedia-system.org**
 
-If you want to contribute to OES please contact the help desk info@open-encyclopedia-system.org.
-
+## Credits
+Developed by **Digitale Infrastrukturen**, Freie Universität Berlin (FUB IT),  
+with support from the **German Research Foundation (DFG)**.
 
 ## Licencing
-
-Copyright (C) 2023 Freie Universität Berlin, Center für Digitale Systeme an der Universitätsbibliothek
-
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+Copyright (C) 2025
+Freie Universität Berlin, FUB IT, Digitale Infrastrukturen
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
 # Changelog
 
-## 1.2.4
-* improve - panel calls to fit OES Core 2.3.3
-* remove - media.css, moved to OES Core
+### 1.3.0
+- Added: Blocks that work without ACF in OES Core
 
-## 1.2.3
-* add - prefix field for gallery title in gallery block
-* add - pdf display
+### 1.2.4
+- Improved: Panel integration for OES Core 2.3.3
+- Removed: `media.css` (moved to OES Core)
 
-## 1.2.2
-* change - remove namespace für \OES\Media\enqueue_scripts
-* improve - pdf display for gallery block
-* fix - gallery slider
+### 1.2.3
+- Added: Prefix field for gallery titles
+- Added: PDF display support
 
-## 1.2.1
-* fix - gallery block
+### 1.2.2
+- Changed: Removed namespace from `\OES\Media\enqueue_scripts`
+- Improved: Gallery block PDF display
+- Fixed: Gallery slider bug
+
+### 1.2.1
+- Fixed: Gallery block behavior
--- oes-media.php
+++ oes-media.php
@@ -1,14 +1,22 @@
 <?php
 
 /**
- * Plugin Name: OES Media (OES Core Module)
- * Plugin URI: http://www.open-encyclopedia-system.org/
- * Description: Display media in an OES context.
- * Version: 1.2.4
- * Author: Maren Welterlich-Strobl, Freie Universität Berlin, Center für Digitale Systeme an der Universitätsbibliothek
- * Author URI: https://www.cedis.fu-berlin.de/cedis/mitarbeiter/beschaeftigte/mstrobl.html
- * License: GPLv2 or later
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * OES Media (Legacy OES Core Module)
+ *
+ * @wordpress-plugin
+ * Plugin Name:        OES Media (Legacy OES Core Module)
+ * Plugin URI:         https://www.open-encyclopedia-system.org/
+ * Description:        Provides media display functionality in OES projects using ACF Pro.
+ *                     Deprecated: This plugin has been replaced by the media blocks in OES Core as of version 2.4.0.
+ * Version:            1.3.0
+ * Author:             Maren Welterlich-Strobl, Freie Universität Berlin, FUB-IT
+ * Author URI:         https://www.it.fu-berlin.de/die-fub-it/mitarbeitende/mstrobl.html
+ * Requires at least:  6.0
+ * Tested up to:       6.8.2
+ * Requires PHP:       7.4
+ * Tags:               oes, media, acf, deprecated, legacy, plugin-addon, encyclopedia
+ * License:            GPLv2 or later
+ * License URI:        https://www.gnu.org/licenses/gpl-2.0.html
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -22,14 +30,16 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
+
+
+namespace OES\Media;
 
 if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
 add_action('oes/plugins_loaded', function () {
 
-    /* check if OES Core Plugin is activated */
     if (!function_exists('OES')) {
         add_action('admin_notices', function () {
             echo '<div class="notice notice-warning is-dismissible"><p>' .
@@ -37,12 +47,273 @@ add_action('oes/plugins_loaded', function () {
         });
     } else {
 
-        /* exit early if OES Plugin was not completely initialized */
         global $oes;
         if (!$oes || !property_exists($oes, 'initialized') || !$oes->initialized) return;
 
-        /* include ACF media blocks */
-        include_once(__DIR__ . '/includes/blocks/functions-blocks.php');
-        add_action('acf/init', '\OES\Media\register_acf_blocks');
+        add_action('acf/init', __NAMESPACE__ . '\register_acf_blocks');
+
+        do_action('oes/media_plugin_loaded');
     }
 }, 14);
+
+/**
+ * Register media blocks with ACF Pro.
+ *
+ * @return void
+ */
+function register_acf_blocks(): void
+{
+    if (function_exists('acf_register_block_type')) {
+
+        acf_register_block_type([
+            'name' => 'oes-panel',
+            'title' => 'OES Panel (ACF Pro)',
+            'render_callback' => '\OES\Media\render_panel',
+            'keywords' => ['OES', 'panel', 'layout'],
+            'supports' => [
+                'align' => true,
+                'anchor' => true,
+                'customClassName' => true,
+                'jsx' => true,
+            ],
+        ]);
+
+        acf_add_local_field_group([
+            'key' => 'group_oes_panel',
+            'title' => 'OES Panel',
+            'fields' => [
+                [
+                    'key' => 'block_field__panel_title',
+                    'label' => 'Title',
+                    'name' => 'panel_title',
+                    'type' => 'text',
+                ],
+                [
+                    'key' => 'block_field__panel_expanded',
+                    'label' => 'Expanded',
+                    'name' => 'panel_expanded',
+                    'type' => 'true_false',
+                    'default_value' => true
+                ]
+            ],
+            'location' => [[[
+                'param' => 'block',
+                'operator' => '==',
+                'value' => 'acf/oes-panel',
+            ]]]
+        ]);
+
+        acf_register_block_type([
+            'name' => 'oes-image-panel',
+            'title' => 'OES Image Panel (ACF Pro)',
+            'render_callback' => '\OES\Media\render_image_panel',
+            'keywords' => ['OES', 'Image', 'Panel'],
+            'mode' => 'auto',
+            'supports' => [
+                'align' => true,
+                'anchor' => true,
+                'customClassName' => true,
+                'jsx' => true,
+            ],
+        ]);
+
+        acf_add_local_field_group([
+            'key' => 'group_oes_image_panel',
+            'title' => 'OES Image Panel',
+            'fields' => [
+                [
+                    'key' => 'field_figure',
+                    'label' => 'Image',
+                    'name' => 'figure',
+                    'type' => 'image',
+                    'return_format' => 'array'
+                ],
+                [
+                    'key' => 'field_figure_title',
+                    'label' => 'Title',
+                    'instructions' => 'Image title if empty, ignore if "none"',
+                    'name' => 'figure_title',
+                    'type' => 'text',
+                ],
+                [
+                    'key' => 'field_figure_number',
+                    'label' => 'Number',
+                    'name' => 'figure_number',
+                    'instructions' => 'Include computed number in panel title',
+                    'type' => 'true_false',
+                    'default_value' => true
+                ],
+                [
+                    'key' => 'field_figure_expanded',
+                    'label' => 'Expanded',
+                    'name' => 'figure_expanded',
+                    'type' => 'true_false',
+                    'default_value' => true
+                ]
+            ],
+            'location' => [[[
+                'param' => 'block',
+                'operator' => '==',
+                'value' => 'acf/oes-image-panel',
+            ]]]
+        ]);
+
+        acf_register_block_type([
+            'name' => 'oes-gallery-panel',
+            'title' => 'OES Gallery Panel (ACF Pro)',
+            'render_callback' => '\OES\Media\render_gallery_panel',
+            'keywords' => ['OES', 'Gallery', 'Panel'],
+            'mode' => 'auto',
+            'supports' => [
+                'align' => true,
+                'anchor' => true,
+                'customClassName' => true,
+                'jsx' => true,
+            ],
+        ]);
+
+        /* @oesLegacy leave repeater field, even though gallery field would be better */
+        acf_add_local_field_group([
+            'key' => 'group_oes_gallery_panel',
+            'title' => 'OES Gallery Panel (ACF Pro)',
+            'fields' => [
+                [
+                    'key' => 'field_gallery_title',
+                    'label' => 'Title',
+                    'instructions' => 'Ignore if "none"',
+                    'name' => 'gallery_title',
+                    'type' => 'text',
+                ],
+                [
+                    'key' => 'field_gallery_number',
+                    'label' => 'Include Numbers in Title',
+                    'name' => 'gallery_number',
+                    'type' => 'true_false',
+                ],
+                [
+                    'key' => 'field_gallery_expanded',
+                    'label' => 'Expanded',
+                    'name' => 'gallery_expanded',
+                    'type' => 'true_false',
+                    'default_value' => true
+                ],
+                [
+                    'key' => 'field_gallery_repeater',
+                    'name' => 'gallery_repeater',
+                    'label' => 'Images',
+                    'type' => 'repeater',
+                    'layout' => 'block',
+                    'collapsed' => 'field_gallery_figure_title',
+                    'sub_fields' => [
+                        [
+                            'key' => 'field_gallery_figure',
+                            'label' => 'Image',
+                            'name' => 'gallery_figure',
+                            'type' => 'image',
+                        ]
+                    ]
+                ],
+            ],
+            'location' => [[[
+                'param' => 'block',
+                'operator' => '==',
+                'value' => 'acf/oes-gallery-panel',
+            ]]]
+        ]);
+    }
+}
+
+/**
+ * Display an OES panel block.
+ *
+ * @param array $block The block data.
+ * @return void
+ */
+function render_panel(array $block): void
+{
+    if ($data = $block['data'] ?? false) {
+        echo '<div class="' . ($block['className'] ?? '') . '" id="' . ($block['anchor'] ?? '') . '">' .
+            oes_get_panel_html('<InnerBlocks />', [
+                'caption' => $data['panel_title'] ?? '',
+                'active' => is_admin() ?
+                    true :
+                    ($data['panel_expanded'] ?? false)
+            ]) .
+            '</div>';
+    }
+}
+
+/**
+ * Display an OES image panel block.
+ *
+ * @param array $block The block data.
+ * @param string $content The block content.
+ * @param bool $is_preview Whether the block is being rendered for editing preview.
+ *
+ * @return void
+ */
+function render_image_panel(array $block, string $content, bool $is_preview): void
+{
+    if ($is_preview) {
+        echo '<span>' . __('[Image Panel, rendered in frontend]', 'oes') . '</span>';
+    }
+    else {
+        $image = $block['data']['figure'] ?? false;
+        if (empty($image) || (is_array($image) && !isset($image['ID']))) {
+            echo '<span>' . __('No valid image selected.', 'oes') . '</span>';
+        }
+        else {
+            echo oes_get_image_panel_html(
+                $image, [
+                'add_number' => $block['data']['figure_number'] ?? true,
+                'caption' => $block['data']['figure_title'] ?? '',
+                'is_expanded' => (bool)($block['data']['figure_expanded'] ?? true)
+            ]);
+        }
+    }
+}
+
+/**
+ * Display an OES gallery panel block.
+ *
+ * @param array $block The block data.
+ * @param string $content The block content.
+ * @param bool $is_preview Whether the block is being rendered for editing preview.
+ *
+ * @return void
+ */
+function render_gallery_panel(array $block, string $content, bool $is_preview): void
+{
+
+    if ($is_preview) {
+        echo '<span>' . __('[Gallery Panel, rendered in frontend]', 'oes') . '</span>';
+    }
+    else {
+
+        $figures = [];
+        $figureNumber = $block['data']['gallery_repeater'] ?? false;
+        if ($figureNumber) {
+            for ($i = 0; $i < $figureNumber; $i++) {
+                if ($imageID = $block['data']['gallery_repeater_' . $i . '_gallery_figure'] ?? false) {
+                    if ($figure = acf_get_attachment($imageID)) {
+                        $figures[] = $figure;
+                    }
+                }
+            }
+        }
+
+        if (empty($figures)) {
+            echo '<span>' . __('No valid images selected.', 'oes') . '</span>';
+        }
+        else {
+            echo oes_get_gallery_panel_html(
+                $figures,
+                [
+                    'caption' => $block['data']['gallery_title'] ?? '',
+                    'add_number' => $block['data']['gallery_number'] ?? false,
+                    'is_expanded' => (bool)($block['data']['gallery_expanded'] ?? true)
+                ]
+            );
+        }
+    }
+}
